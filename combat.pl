25ab8b49eac299766a18252d601779181829b7dc
calafite <pertinaciouslymedici@gmail.com>
2026-07-21 20:47:03 +0100

    refactor: modularise structure further

--- combat.pl
+++ combat.pl
@@ -2,6 +2,7 @@
 
 :- use_module(config).
 :- use_module(entity).
+:- use_module(world).
 
 hostile_at(A, B) :-
     alive(A),
@@ -10,17 +11,17 @@ hostile_at(A, B) :-
     room(B, RId),
     hostile(A, B).
 
-step_kill(S, AId, TId, NS, Evts) :-
-    has(S, AId, A),
-    has(S, TId, T),
+step_kill(W, AId, TId, NW, Evts) :-
+    world:entity(W, AId, A),
+    world:entity(W, TId, T),
     hostile_at(A, T),
     wpn(A, Wpn),
     dmg(Wpn, Dmg),
-    apply_dmg(S, A, T, Dmg, NS, Evts, hit(AId, TId, Dmg)).
+    apply_dmg(W, A, T, Dmg, NW, Evts, hit(AId, TId, Dmg)).
 
-step_cast(S, AId, Sp, TId, NS, Evts) :-
-    has(S, AId, A),
-    has(S, TId, T),
+step_cast(W, AId, Sp, TId, NW, Evts) :-
+    world:entity(W, AId, A),
+    world:entity(W, TId, T),
     hostile_at(A, T),
     cost(Sp, Cost),
     mp(A, Mp),
@@ -28,30 +29,30 @@ step_cast(S, AId, Sp, TId, NS, Evts) :-
     NMp is Mp - Cost,
     mp(A, NMp, MidA),
     dmg(Sp, Dmg),
-    apply_dmg(S, MidA, T, Dmg, NS, Evts, cast(AId, Sp, TId, Dmg)).
+    apply_dmg(W, MidA, T, Dmg, NW, Evts, cast(AId, Sp, TId, Dmg)).
 
-apply_dmg(S, A, T, Dmg, NS, [HitEvt, dead(TId) | REvts], HitEvt) :-
+apply_dmg(W, A, T, Dmg, NW, [HitEvt, dead(TId) | REvts], HitEvt) :-
     hp(T, THp),
     NTHp is THp - Dmg,
     NTHp =< 0, !,
     hp(T, 0, NT),
     TId = NT.id,
-    reward(S, A, NT, NS, REvts).
+    reward(W, A, NT, NW, REvts).
 
-apply_dmg(S, A, T, Dmg, NS, [HitEvt], HitEvt) :-
+apply_dmg(W, A, T, Dmg, NW, [HitEvt], HitEvt) :-
     hp(T, THp),
     NTHp is THp - Dmg,
     hp(T, NTHp, NT),
-    put(S, A.id, A, TS),
-    put(TS, T.id, NT, NS).
+    world:update(W, A, TW),
+    world:update(TW, NT, NW).
 
-reward(S, A, mob{id: MId}, NS, [lvl_up(A.id, NLvl)]) :-
+reward(W, A, mob{id: MId}, NW, [lvl_up(A.id, NLvl)]) :-
     lvl(A, Lvl),
     NLvl is Lvl + 1,
     lvl(A, NLvl, NA),
-    del(S, MId, TS),
-    put(TS, A.id, NA, NS).
+    world:remove(W, MId, TW),
+    world:update(TW, NA, NW).
 
-reward(S, A, plyr{id: PId} = NT, NS, []) :-
-    put(S, A.id, A, TS),
-    put(TS, PId, NT, NS).
+reward(W, A, plyr{id: PId} = NT, NW, []) :-
+    world:update(W, A, TW),
+    world:update(TW, NT, NW).
