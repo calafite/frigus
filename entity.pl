fa488f2e150f6a64de6b26466716035833fb7849
calafite <pertinaciouslymedici@gmail.com>
2026-07-21 20:45:58 +0100

    refactor: phase out list manipulation

--- entity.pl
+++ entity.pl
@@ -1,43 +1,17 @@
 :- module(entity, [
     hp/2, hp/3, room/2, room/3, mp/2, mp/3,
     lvl/2, lvl/3, inv/2, inv/3, equip/2, equip/3,
-    wpn/2, alive/1, has/3, node/3, put/4, del/3
+    wpn/2, alive/1
 ]).
 
-hp(E, E.hp).
-hp(E, V, E.put(hp, V)).
-
-room(E, E.room).
-room(E, V, E.put(room, V)).
-
-mp(E, E.mp).
-mp(E, V, E.put(mp, V)).
-
-lvl(E, E.lvl).
-lvl(E, V, E.put(lvl, V)).
-
-inv(E, E.inv).
-inv(E, V, E.put(inv, V)).
-
-equip(E, E.equip).
-equip(E, V, E.put(equip, V)).
+hp(E, E.hp).         hp(E, V, E.put(hp, V)).
+room(E, E.room).     room(E, V, E.put(room, V)).
+mp(E, E.mp).         mp(E, V, E.put(mp, V)).
+lvl(E, E.lvl).       lvl(E, V, E.put(lvl, V)).
+inv(E, E.inv).       inv(E, V, E.put(inv, V)).
+equip(E, E.equip).   equip(E, V, E.put(equip, V)).
 
 wpn(E, W) :- is_dict(E, plyr), get_dict(wpn, E.equip, W), W \== none, !.
 wpn(_, fists).
 
 alive(E) :- hp(E, Hp), Hp > 0.
-
-find_id(Id, L, E, R) :- select(E, L, R), E.id == Id, !.
-
-has(S, Id, E) :- member(E, S.plyrs), E.id == Id, !.
-has(S, Id, E) :- member(E, S.mobs), E.id == Id, !.
-has(S, Id, E) :- member(E, S.items), E.id == Id, !.
-
-node(S, Id, Node) :- member(Node, S.rooms), Node.id == Id, !.
-
-put(S, Id, E, NS) :- find_id(Id, S.plyrs, _, R), !, NS = S.put(plyrs, [E|R]).
-put(S, Id, E, NS) :- find_id(Id, S.mobs, _, R), !, NS = S.put(mobs, [E|R]).
-put(S, Id, E, NS) :- find_id(Id, S.items, _, R), !, NS = S.put(items, [E|R]).
-
-del(S, Id, NS) :- find_id(Id, S.mobs, _, R), !, NS = S.put(mobs, R).
-del(S, Id, NS) :- find_id(Id, S.items, _, R), !, NS = S.put(items, R).
