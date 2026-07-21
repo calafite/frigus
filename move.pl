94eaeeaa134d091fb34f34c1dfe42cee1a642910
calafite <pertinaciouslymedici@gmail.com>
2026-07-21 20:43:39 +0100

    refactor: adjust to new map and world mechanics

--- move.pl
+++ move.pl
@@ -1,11 +1,19 @@
 :- module(move, [step_move/5]).
 
+:- use_module(world).
 :- use_module(entity).
+:- use_module(map).
 
-step_move(S, Id, Dir, NS, [moved(Id, Dir, NRId)]) :-
-    has(S, Id, A),
-    room(A, RId),
-    node(S, RId, Node),
+step_move(W, Id, Dir, NW, [moved(Id, Dir, NRId) | SideEvts]) :-
+    world:entity(W, Id, A),
+    entity:room(A, RId),
+    world:node(W, RId, Node),
+
     get_dict(Dir, Node.exits, NRId),
-    room(A, NRId, NA),
-    put(S, Id, NA, NS).
+    world:node(W, NRId, NextNode),
+
+    map:can_enter(W, A, NextNode),
+    entity:room(A, NRId, NA),
+    map:on_enter(W, NA, NextNode, NNA, SideEvts),
+
+    world:update(W, NNA, NW).
